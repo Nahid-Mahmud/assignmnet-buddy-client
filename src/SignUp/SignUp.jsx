4b6a3e99c5e98d643ecfb65e3e04b622084e606b
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2023-11-05 03:28:01 +0600

    Google Sign up completed

--- src/SignUp/SignUp.jsx
+++ src/SignUp/SignUp.jsx
@@ -1,21 +1,22 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../Hooks/useAuth";
-import { toast } from "react-toastify";
+import { toast, useToast } from "react-toastify";
 import { useState } from "react";
 import { updateProfile } from "firebase/auth";
+import GoogleLogin from "../SharedComponents/GoogleLogin";
 
 const SignUp = () => {
   // useSates
-  const [signUpError, setSignUpError] = useState("");
+  const [userSignUpError, SetUserSignUpError] = useState("");
   // values form authprovider
   const { googleLogIn, emailPassSignup } = useAuth();
 
-
   // navigate user
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   // handle form function
   const handleForm = (e) => {
+    SetUserSignUpError("");
     e.preventDefault();
     const name = e.target.name.value;
     const photoUrl = e.target.photourl.value;
@@ -24,6 +25,17 @@ const SignUp = () => {
     // console.log(name,photoUrl,email,password);
 
     // password validate
+
+    if (!/^.{6,}$/.test(password)) {
+      SetUserSignUpError(`Must have at least 6 characters`);
+      return;
+    } else if (!/^(?=.*[A-Z]).+$/.test(password)) {
+      SetUserSignUpError(`Must have at least 1 capital letter`);
+      return;
+    } else if (!/^(?=.*[!@#$%^&*()_+{}[\]:;<>,.?/~])\S+$/.test(password)) {
+      SetUserSignUpError(`Must have at least 1 special character`);
+      return;
+    }
 
     // create user with email password
     emailPassSignup(email, password)
@@ -39,25 +51,53 @@ const SignUp = () => {
           .catch((err) => {
             console.log(err);
           });
-          if(currentUser){
-            toast(" Sign Up Successfull", {
-                position: "top-center",
-                autoClose: 5000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-                theme: "light",
-              });
-              navigate("/");
-          }
+        if (currentUser) {
+          toast(" Sign Up Successfull", {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+          });
+          navigate("/");
+          //   e.target.reset();
+        }
       })
       .catch((err) => {
         console.log(err);
-        setSignUpError(err.message);
+        SetUserSignUpError(err.message);
       });
   };
+
+  const handleGoogleLogin = () => {
+    SetUserSignUpError("");
+    console.log("first");
+    googleLogIn()
+      .then((result) => {
+        const currentUser = result.user;
+        if (currentUser) {
+          toast(" Sign Up Successfull", {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+          });
+          navigate("/");
+          //   e.target.reset();
+        }
+      })
+      .catch((err) => {
+        SetUserSignUpError(err);
+      });
+  };
+
   return (
     <div
       className="hero min-h-screen"
@@ -66,79 +106,81 @@ const SignUp = () => {
       }}
     >
       <div className="w-full max-w-md">
-        <form
-          onSubmit={handleForm}
-          className="bg-white bg-opacity-50 shadow-md rounded px-8 pt-6 pb-8 mb-4"
-        >
-          <p className="text-center  text-3xl font-semibold underline py-5">
-            Create Your Account
-          </p>
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2">
-              Your Name
-            </label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="username"
-              type="text"
-              name="name"
-              placeholder="Jhon Doe"
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2">
-              Photo Url
-            </label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="photourl"
-              type="text"
-              name="photourl"
-              placeholder="https://example.png"
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2">
-              Email
-            </label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="email"
-              type="email"
-              name="email"
-              placeholder="example@gmail.com"
-            />
-          </div>
-          <div className="mb-6">
-            <label className="block text-gray-700 text-sm font-bold mb-2">
-              Password
-            </label>
-            <input
-              className="shadow appearance-none border  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-              id="password"
-              type="password"
-              placeholder="******************"
-            />
-            <p className="text-red-500 text-xs italic">{signUpError}</p>
-          </div>
-          <div className="flex items-center justify-between">
-            <input
-              className="btn bg-green-600 text-white hover:text-black hover:bg-green-500 transition"
-              type="submit"
-              value="Sign Up"
-            />
-            <p className="inline-block align-baseline font-bold  " href="#">
-              Have an account?{" "}
-              <Link
-                className="text-blue-500  hover:text-blue-800"
-                to={"/signin"}
-              >
-                SignIn
-              </Link>{" "}
-              here
+        <div className="bg-white bg-opacity-50 shadow-md rounded px-8 pt-6 pb-8 mb-4">
+          <form onSubmit={handleForm}>
+            <p className="text-center  text-3xl font-semibold underline py-5">
+              Create Your Account
             </p>
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2">
+                Your Name
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="username"
+                type="text"
+                name="name"
+                placeholder="Jhon Doe"
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2">
+                Photo Url
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="photourl"
+                type="text"
+                name="photourl"
+                placeholder="https://example.png"
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2">
+                Email
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="email"
+                type="email"
+                name="email"
+                placeholder="example@gmail.com"
+              />
+            </div>
+            <div className="mb-6">
+              <label className="block text-gray-700 text-sm font-bold mb-2">
+                Password
+              </label>
+              <input
+                className="shadow appearance-none border  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                id="password"
+                type="password"
+                placeholder="******************"
+              />
+              <p className="text-red-500 text-xs italic">{userSignUpError}</p>
+            </div>
+            <div className="flex items-center justify-between">
+              <input
+                className="btn bg-green-600 text-white hover:text-black hover:bg-green-500 transition"
+                type="submit"
+                value="Sign Up"
+              />
+              <p className="inline-block align-baseline font-bold  " href="#">
+                Have an account?{" "}
+                <Link
+                  className="text-blue-500  hover:text-blue-800"
+                  to={"/signin"}
+                >
+                  SignIn
+                </Link>{" "}
+                here
+              </p>
+            </div>
+          </form>
+          <div onClick={handleGoogleLogin} className="text-center py-7 ">
+            <GoogleLogin text="Sign Up With Google "></GoogleLogin>
           </div>
-        </form>
+        </div>
         <p className="text-center text-slate-300 text-xs">
           &copy;2023 Assignment Buddy. All rights reserved.
         </p>
