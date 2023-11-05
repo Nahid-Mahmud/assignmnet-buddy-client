a0a8c474d6e83b5a8ccf92233c648d51c6a64296
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2023-11-05 23:56:39 +0600

    Create assignment page farmer motion animation added

--- src/Pages/CreateAssignment/CreateAssignment.jsx
+++ src/Pages/CreateAssignment/CreateAssignment.jsx
@@ -1,11 +1,28 @@
-
-
+import { motion, useTime, useTransform } from "framer-motion";
 const CreateAssignment = () => {
-    return (
-        <div>
-            Create Assignment
-        </div>
-    );
+  const time = useTime();
+  const rotate = useTransform(time, [0, 4000], [0, 360], { clamp: false });
+  return (
+    <div className="md:max-w-[90vw] max-w-[95vw] py-10 mx-auto">
+      <div className="flex justify-center">
+        <motion.div
+          style={{ rotate }}
+          className="bg-[#ff4e59] rounded-[2rem] w-36 h-36"
+        ></motion.div>
+        <motion.div
+          style={{ rotate }}
+          className="bg-[#245d51] rounded-[2rem] w-36 h-36"
+        ></motion.div>
+        <motion.div
+          style={{ rotate }}
+          className="bg-[#ff4e59] rounded-[2rem] w-36 h-36"
+        ></motion.div>
+      </div>
+      <p className="capitalize text-center py-10 text-2xl md:text-3xl font-bold underline">
+        Creaate an assignment.
+      </p>
+    </div>
+  );
 };
 
 export default CreateAssignment;
