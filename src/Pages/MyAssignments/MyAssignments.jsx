4d7fd69caf5fbe311cb781aa11eeeae065187e6c
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2023-11-07 23:30:44 +0600

    My assignment page was causing error for axios and react query . code refactored and used simple useeffect and fetch

--- src/Pages/MyAssignments/MyAssignments.jsx
+++ src/Pages/MyAssignments/MyAssignments.jsx
@@ -3,39 +3,54 @@ import Loader from "../../SharedComponents/Loader";
 import axios from "axios";
 import { useAuth } from "../../Hooks/useAuth";
 import MyAssignmentCard from "./MyAssignmentCard";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Swal from "sweetalert2";
 
 const MyAssignments = () => {
   // const geeting current user forom auth contenx
   const { user } = useAuth();
+  const [myassignments, setmyassignments] = useState([]);
+  const baseUrl = `${import.meta.env.VITE_serverUrl}/allAssignment/user?email=${
+    user.email
+  }`;
+  useEffect(() => {
+    fetch(baseUrl)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("inside fetch", data);
+        setmyassignments(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [baseUrl]);
 
-  // getting data from server
-  const { data, isLoading, isError } = useQuery({
-    queryKey: ["usercreatedAssignments"],
-    queryFn: async () => {
-      const result = await axios.get(
-        `${import.meta.env.VITE_serverUrl}/allAssignment/user?email=${
-          user.email
-        }`
-      );
-      const userCreatedAssignments = result.data;
-      return userCreatedAssignments;
-    },
-    retry: 2,
-  });
-  console.log(data, isLoading, isError);
-  // is loading
-  if (isLoading) {
-    return <Loader></Loader>;
-  }
-  // is error
-  if (isError) {
-    return <div>Can't Get Data From Server</div>;
-  }
+  // // getting data from server
+  // const { data, isLoading, isError } = useQuery({
+  //   queryKey: ["usercreatedAssignments"],
+  //   queryFn: async () => {
+  //     const result = await axios.get(
+  //       `${import.meta.env.VITE_serverUrl}/allAssignment/user?email=${
+  //         user.email
+  //       }`
+  //     );
+  //     const userCreatedAssignments = result.data;
+  //     return userCreatedAssignments;
+  //   },
+  //   retry: 2,
+  // });
+  // console.log(data, isLoading, isError);
+  // // is loading
+  // if (isLoading) {
+  //   return <Loader></Loader>;
+  // }
+  // // is error
+  // if (isError) {
+  //   return <div>Can't Get Data From Server</div>;
+  // }
 
   // use sates
-  const [usercreatedAssignments, setUsercreatedAssignments] = useState(data);
+  // const [usercreatedAssignments, setUsercreatedAssignments] = useState(data);
 
   //delete assignment
   const handleDelete = (id) => {
@@ -50,8 +65,8 @@ const MyAssignments = () => {
             title: "Successful",
             text: "Assignment Deleted Successfully !",
           });
-          const filterData = data.filter((data) => data._id !== id);
-          setUsercreatedAssignments(filterData);
+          const filterData = myassignments.filter((data) => data._id !== id);
+          setmyassignments(filterData);
         }
       });
   };
@@ -65,7 +80,7 @@ const MyAssignments = () => {
           </h1>
         </div>
         <div className="min-h-[60vh] grid gap-5 md:grid-cols-2 lg:grid-cols-3  xl:grid-cols-4 py-16 ">
-          {usercreatedAssignments.map((assignmet, index) => (
+          {myassignments.map((assignmet, index) => (
             <MyAssignmentCard
               handleDelete={handleDelete}
               key={index}
